9092bdb2fa2da8d2fa6e8f200f418ef3880da259
YangDamin <cxz26@naver.com>
2022-01-27 19:13:26 +0900

    간단한 회원가입 구현

---
+++ mini3_spring/src/main/java/com/example/mini3_spring/model/User.java
@@ -0,0 +1,19 @@
+package com.example.mini3_spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+    String email;
+    String pwd;
+}
